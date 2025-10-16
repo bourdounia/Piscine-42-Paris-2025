2c749848953e6b265c5d0f4c024b6f285fcbb1cd
Bour Dounia <112276337+bourdounia@users.noreply.github.com>
2025-10-16 07:05:52 +0000

    1

--- EXAMS/aff_a.c
+++ EXAMS/aff_a.c
@@ -1,6 +1,6 @@
 /*Nom de l'affectation : aff_a
 Fichiers attendus : aff_a.c
-Fonctions autorisées : écriture
+Fonctions autorisées : write
 --------------------------------------------------------------------------------
 
 Écrivez un programme qui prend une chaîne et affiche le premier caractère « a », suivi d'un saut de ligne. Si la chaîne ne contient aucun caractère « a », le programme écrit simplement un saut de ligne. Si le nombre de paramètres est différent de 1, le programme affiche « a » suivi d'un saut de ligne.*/
