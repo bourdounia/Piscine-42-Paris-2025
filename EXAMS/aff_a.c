e4e4f2617198489a71c620f65e9e3e57f3589ea1
Bour Dounia <112276337+bourdounia@users.noreply.github.com>
2025-10-16 05:51:13 +0000

    1

---
+++ EXAMS/aff_a.c
@@ -0,0 +1,14 @@
+/*Nom de l'affectation : aff_a
+Fichiers attendus : aff_a.c
+Fonctions autorisées : écriture
+--------------------------------------------------------------------------------
+
+Écrivez un programme qui prend une chaîne et affiche le premier caractère « a », suivi d'un saut de ligne. Si la chaîne ne contient aucun caractère « a », le programme écrit simplement un saut de ligne. Si le nombre de paramètres est différent de 1, le programme affiche « a » suivi d'un saut de ligne.*/
+
+#include <unistd.h>
+
+int main(void)
+{
+    write(1, "a\n", 2);
+    return (0);
+}
