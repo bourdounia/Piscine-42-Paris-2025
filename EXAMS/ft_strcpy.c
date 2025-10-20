7e1b4b153337cd71fd5cd6acd5c0d2b48d9c1795
Bour Dounia <112276337+bourdounia@users.noreply.github.com>
2025-10-20 19:13:55 +0000

    1

---
+++ EXAMS/ft_strcpy.c
@@ -0,0 +1,27 @@
+/*
+Nom de l'affectation : ft_strcpy
+Fichiers attendus : ft_strcpy.c
+Fonctions autorisées :
+--------------------------------------------------------------------------------
+
+Воспроизведите поведение функции strcpy (man strcpy).
+
+
+Cette fonction est juste pour vous aider à résoudre le problème :
+
+char *ft_strcpy(char *s1, char *s2);
+*/
+
+char    *ft_strcpy(char *s1, char *s2)
+{
+    int i;
+
+    i = 0;
+    while (s2[i])
+    {
+        s1[i] = s2[i];
+        i++;
+    }
+    s1[i] = '\0';
+    return (s1);
+}
